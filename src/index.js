35c781c702fbacef6696352beb77fc025402d067
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-07-01 22:37:39 +0530

    (init) created a todoItem factory function
    - sets title, description, dueDate and prio
    - returns a show() method to display the details

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+function todoItem(title, desc, dueDate, prio){
+  this.title = title;
+  this.description = desc;
+  this.dueDate = dueDate;
+  this.prio = prio;
+
+  function show(){
+    console.log(title);
+    console.log(description);
+    console.log(dueDate);
+    console.log(prio);
+  }
+
+  return {show}
+}
+
+item1 = todoItem("create To Do App", "To create an application to track tasks", "4-6-25", "usual");
+item1.show();
