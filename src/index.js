d30fa93f70f0c63d0482874c236547b230cec4ca
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-07-10 21:23:12 +0530

    (feat) adjusting grid display for tablet and mobile screen sizes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Task } from "./task.js"
 import { createTaskListDOM } from "./task_list_display.js"
 import { addProjectEventListeners } from "./add_project_el.js"
 
-let portfolio, p1, item1, job, tl, date;
+let portfolio, p1, item1, job, tl, date, landing_page;
 
 document.querySelector("body").appendChild(nav)
 portfolio = new Portfolio("Briefcase");
@@ -14,12 +14,15 @@ p1 = new Project("Magic");
 portfolio.addProject(p1);
 
 date = {dd: 4, mm: 12, yy: 25}
-item1 = new Task("To do app", `1.date progress bar in cards`,  date,"low");
+item1 = new Task("To do app", `1.date progress bar in cards\n2.Rock Pile up for tasks in navbar`,  date,"low");
 date = {dd: 23, mm: 10, yy: 22}
 job = new Task("Library App", "1.Styling of content\n\n2.Styling", date, "medium");
+date = {dd: 1, mm: 8, yy: 25}
+landing_page =new Task("Edit task" ,`display form to capture task edit`, date, "high")
 
 p1.addTask(item1);
 p1.addTask(job);
+p1.addTask(landing_page)
 
 tl = createTaskListDOM(portfolio);
 document.querySelector("body").appendChild(tl);
